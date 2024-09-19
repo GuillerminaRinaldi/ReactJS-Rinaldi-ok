1873445117940cfa9fa4a70c207e291ff43fbe3a
guillerminarinaldi <guillerinaldi@hotmail.com>
2024-09-19 10:10:03 -0500

    config firebase

---
+++ src/firebase/database.js
@@ -0,0 +1,9 @@
+import { getFirestore, collection, getDocs } from "firebase/firestore";
+import { app } from './config'
+
+const db = getFirestore(app);
+
+const querySnapshot = await getDocs(collection(db, "users"));
+querySnapshot.forEach((doc) => {
+  console.log(`${doc.id} => ${doc.data()}`);
+});
